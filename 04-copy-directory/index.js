72f7c0f9aeba69c1093f4002f779afb864a06be2
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-11-09 00:55:54 +0100

    feat: add deep copy dir in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,46 +1,31 @@
 
 const fs = require("fs/promises");
 const path = require("path");
-
+const { stderr } = require("node:process");
 const pathToDestDir = path.join(__dirname, "files-copy");
 const pathToSrcDir = path.join(__dirname, "files");
 
 async function copyDir (src, dest) {
   try {
+    // delete all old files in copy dir (it is better, because chekiang names of files could work only if we change nothing inside file)
+    await fs.rm(dest, { recursive: true, force: true})
     // create dir, if dir don't exist:
     await fs.mkdir(dest, {recursive: true});
-    // read files in both directories:
-    const filesDest = await fs.readdir(dest, {withFileTypes: true});
+    // read files in src dir:
     const filesSrc = await fs.readdir(src, {withFileTypes: true});
-    // delete all old files in copy dir (it is better, because chekiang names of files could work only if we change nothing inside file)
-    filesDest.forEach((file) => {
-      const destPath = path.join(dest, file.name);
-      if(file.isDirectory()) {
-          // fs.readdir(destPath).then(res => {
-          //   if(res.length === 0) {
-          //     fs.rmdir(destPath);
-          //   } else{
-          //     fs.unlink(path.join(destPath, res[0]))
-          //   }
-          // });
-      } else {
-        fs.unlink(destPath);
-      };
-    });
-
     // copy all files in dest dir:
     filesSrc.forEach(file => {
           const srcPath = path.join(src, file.name);
           const destPath = path.join(dest, file.name);
 
           if(file.isDirectory()) {
-            //TODO: deep files copy
+            copyDir (srcPath, destPath)
           } else {
-              fs.copyFile(srcPath, destPath);
+            fs.copyFile(srcPath, destPath);
           }
     });
   } catch (error) {
-    console.log(error);
+    stderr.write(error)
   }
 };
 
